88ddaec8a7037b46d1714d47262f2f0492d3ed3b
jgierlach <jegierlach@gmail.com>
2018-08-02 17:35:05 -0500

    Added button and textfield

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput } from 'react-native';
+import { Container, Header, Content, Input, Item, Form, Textarea, Button } from 'native-base';
 
 export default class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      text: ''
+    }
+  }
   render() {
+    console.log('Our apps state', this.state)
     return (
       <View style={styles.container}>
         <Image source={require('./public/subText_logo.png')} style={styles.image}/>
+          <Form rounded style={{marginTop: 40, marginLeft: 20, marginRight: 20}}> 
+            <Textarea rowSpan={8} style={{fontSize: 20}} placeholder="Find out what they think you'll mean"/>
+            <Button block info style={{marginTop: 200}}>
+              <Text style={{color: 'white'}}>Generate Report</Text>
+            </Button>
+          </Form>
       </View>
     );
   }
