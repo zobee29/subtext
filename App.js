4cd6a888d3b1e30f858e62d0416cdcb248c43ddb
zfarooqi <farooqi1992@gmail.com>
2018-08-03 10:56:25 -0500

    Readjusted our styles sheet, and we successfully hooked up our button to our api calls.

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image, TextInput } from 'react-native';
 import { Container, Header, Content, Input, Item, Form, Textarea, Button } from 'native-base';
+import axios from 'axios'
 
 export default class App extends React.Component {
   constructor() {
@@ -9,32 +10,65 @@ export default class App extends React.Component {
       text: ''
     }
   }
+
+  
+sendData = async (text) => {
+  try{
+  const data = await axios.post(
+    'https://apiv2.indico.io/sentimenthq',
+    JSON.stringify({
+      'api_key': "3f58c49779f1a00d19e00cc8feaed15a",
+      'data': text
+    })
+  )
+  console.log(data)
+  } catch (error){
+    console.error(error)
+  }
+}
+
   render() {
     console.log('Our apps state', this.state)
     return (
       <View style={styles.container}>
-        <Image source={require('./public/subText_logo.png')} style={styles.image}/>
-          <Form rounded style={{marginTop: 40, marginLeft: 20, marginRight: 20}}> 
-            <Textarea rowSpan={8} style={{fontSize: 20}} placeholder="Find out what they think you'll mean"/>
-            <Button block info style={{marginTop: 200}}>
-              <Text style={{color: 'white'}}>Generate Report</Text>
+        <Image source={require('./public/subText_logo.png')} style={styles.logo}/>
+          <Form rounded style={styles.form}> 
+            <Textarea rowSpan={8} style={styles.textArea} placeholder="Find out what they think you'll mean" onChangeText={(text) => this.setState({text})}/>
+            <Button block info style={styles.button} onPress={()=>this.sendData(this.state.text)}>
+              <Text style={styles.buttonText}>Generate Report</Text>
             </Button>
           </Form>
       </View>
     );
   }
 }
-
+ 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 2,
+    flexDirection: 'column',
     backgroundColor: 'white',
     alignItems: 'center',
-    // justifyContent: 'center',
+    justifyContent: 'space-evenly',
   },
-  image: {
+  logo: {
     width: 200,
     height: 100,
-    marginTop: 50,
+    marginBottom: 60
+    // marginTop: 50,
+  },
+  form: {
+    // marginTop: 40,
+    marginHorizontal: 20,
+  },
+  textArea: {
+    fontSize: 20
+  },
+  button: {
+    
+  },
+  buttonText: {
+    color: 'white',
+    marginHorizontal: 40
   }
 });
